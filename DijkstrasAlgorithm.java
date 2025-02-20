e171509a02ff22171a75f087d8bbc386131723cd
fowblemason <fowblemason@gmail.com>
2025-02-20 14:21:16 -0800

    Update DijkstrasAlgorithm.java
    Rough outline of work we worked through in class

--- DijkstrasAlgorithm.java
+++ DijkstrasAlgorithm.java
@@ -1,1 +1,33 @@
+class Main {
+    public static void main(String[] args) {
+        
+    }
+    ShortestPath(Graph g) {
+        for(int i = 0; i < number_vertex; i++) {
+            
+        }
+    }
+}
+int numberVertices
+int numberEdges
+int graph[numberVertices][numberVertices]
+String vertexNames[numberVertices]
 
+//Vertex[numberVertices][] graph
+
+class Vertex {
+    String name
+    String neighbors[]
+    int pathWeights[] //weight of path from you to a neighbor
+}
+
+/**
+May or may not be a connected graph
+assume undirected
+all weights are positive
+output: full path and weight 
+every shortest path
+just output the shortest paths 
+
+store shortest paths in an array as we find them
+*/ 
